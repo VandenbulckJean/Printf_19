9bdadaa74dd98288e4dc8d94e8c2af38cbfa5eb5
Jean Vanden Bulck <jeanvandenbulck@MacBook-Pro-de-Jean.local>
2020-12-02 15:37:47 +0100

    dd

--- conversion_di_2.c
+++ conversion_di_2.c
@@ -6,28 +6,30 @@
 /*   By: jvanden- <jvanden-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 12:26:55 by jvanden-          #+#    #+#             */
-/*   Updated: 2020/12/02 15:34:55 by jvanden-         ###   ########.fr       */
+/*   Updated: 2020/12/02 15:36:49 by jvanden-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "printf.h"
+#include "printf.h"
 
 int		processing_d_width_minus_precison(t_fnc_data *data, int negative)
 {
 	char *str;
 
-	if (!(str = create_filled_string(data->amount_precision - ft_strlen(data->string), '0')))
+	if (!(str = create_filled_string(data->amount_precision -
+	ft_strlen(data->string), '0')))
 		return (-1);
 	if (!(data->string = ft_strjoin_front(data->string, str)))
-			return (memory_allocation_error_free(str));
+		return (memory_allocation_error_free(str));
 	free(str);
 	if (negative)
 		if (!(data->string = ft_strjoin_front(data->string, "-")))
 			return (-1);
-	if (!(str = create_filled_string(data->width - ft_strlen(data->string), ' ')))
+	if (!(str = create_filled_string(data->width -
+	ft_strlen(data->string), ' ')))
 		return (-1);
 	if (!(data->string = ft_strjoin_back(data->string, str)))
-			return (memory_allocation_error_free(str));
+		return (memory_allocation_error_free(str));
 	free(str);
 	return (1);
 }
---
+++ conversion_uxxx.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conversion_uxX.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvanden- <jvanden-@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/01 13:21:20 by jvanden-          #+#    #+#             */
+/*   Updated: 2020/12/02 15:37:08 by jvanden-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+int	processing_u(t_fnc_data *data)
+{
+	char			*str;
+	unsigned int	value;
+
+	value = va_arg(data->saved_variables, unsigned int);
+	if (data->precision && value == 0)
+	{
+		if (!(str = malloc(sizeof(char) * 1)))
+			return (-1);
+		str[1] = '\0';
+	}
+	else
+	{
+		if (!(str = ft_itoa(value, 10, "0123456789")))
+			return (-1);
+		if (!(data->string = ft_strjoin_back(data->string, str)))
+			return (memory_allocation_error_free(str));
+	}
+	free(str);
+	return (processing_d_flags(data, 0));
+}
+
+int	processing_x(t_fnc_data *data)
+{
+	char			*str;
+	unsigned int	value;
+
+	value = va_arg(data->saved_variables, unsigned int);
+	if (data->precision && value == 0)
+	{
+		if (!(str = malloc(sizeof(char) * 1)))
+			return (-1);
+		str[1] = '\0';
+	}
+	else
+	{
+		if (!(str = ft_itoa(value, 16, "0123456789abcdef")))
+			return (-1);
+		if (!(data->string = ft_strjoin_back(data->string, str)))
+			return (memory_allocation_error_free(str));
+	}
+	free(str);
+	return (processing_d_flags(data, 0));
+}
+
+int	processing_cap_x(t_fnc_data *data)
+{
+	char			*str;
+	unsigned int	value;
+
+	value = va_arg(data->saved_variables, unsigned int);
+	if (data->precision && value == 0)
+	{
+		if (!(str = malloc(sizeof(char) * 1)))
+			return (-1);
+		str[1] = '\0';
+	}
+	else
+	{
+		if (!(str = ft_itoa(value, 16, "0123456789ABCDEF")))
+			return (-1);
+		if (!(data->string = ft_strjoin_back(data->string, str)))
+			return (memory_allocation_error_free(str));
+	}
+	free(str);
+	return (processing_d_flags(data, 0));
+}
