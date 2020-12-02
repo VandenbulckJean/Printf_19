d6ad3b9788d371031de8a70f9f384427923261d7
Jean Vanden Bulck <jeanvandenbulck@MacBook-Pro-de-Jean.local>
2020-12-02 13:50:01 +0100

    Norme15

--- conversion_percent.c
+++ conversion_percent.c
@@ -6,24 +6,27 @@
 /*   By: jvanden- <jvanden-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 13:21:06 by jvanden-          #+#    #+#             */
-/*   Updated: 2020/12/02 13:48:34 by jvanden-         ###   ########.fr       */
+/*   Updated: 2020/12/02 13:49:57 by jvanden-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "printf.h"
+#include "printf.h"
 
-int			processing_percent_width_minus_flag(va_list saved_variables, t_fnc_data *data)
+int			processing_percent_width_minus_flag(
+		va_list saved_variables, t_fnc_data *data)
 {
 	char *str;
+	
 	if (!(str = create_filled_string(data->width - 1, ' ')))
 		return (-1);
 	if (!(data->string = ft_strjoin_back(data->string, str)))
 		return (Memory_allocation_error_free(str));
 	free(str);
-	return(1);
+	return (1);
 }
 
-int			processing_percent_width_zero_flag(va_list saved_variables, t_fnc_data *data)
+int			processing_percent_width_zero_flag(
+		va_list saved_variables, t_fnc_data *data)
 {
 	char *str;
 
@@ -32,7 +35,7 @@ int			processing_percent_width_zero_flag(va_list saved_variables, t_fnc_data *da
 	if (!(data->string = ft_strjoin_front(data->string, str)))
 		return (Memory_allocation_error_free(str));
 	free(str);
-	return(1);
+	return (1);
 }
 
 int			processing_percent_width_flag(va_list width, t_fnc_data *data)
