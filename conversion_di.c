a19464ca8ff496b273cf0e18cba3b4474e7e99e3
Jean Vanden Bulck <jeanvandenbulck@MacBook-Pro-de-Jean.local>
2020-12-02 12:59:12 +0100

    Norme3

--- conversion_di.c
+++ conversion_di.c
@@ -6,14 +6,14 @@
 /*   By: jvanden- <jvanden-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 13:20:54 by jvanden-          #+#    #+#             */
-/*   Updated: 2020/12/02 12:57:33 by jvanden-         ###   ########.fr       */
+/*   Updated: 2020/12/02 12:59:03 by jvanden-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
 int				processing_d_precision_flag(va_list
- saved_variables, t_fnc_data *data, int negative)
+	saved_variables, t_fnc_data *data, int negative)
 {
 	char *str;
 
@@ -26,11 +26,11 @@ int				processing_d_precision_flag(va_list
 	if (negative)
 		if (!(data->string = ft_strjoin_front(data->string, "-")))
 			return (-1);
-	return(1);
+	return (1);
 }
 
 int				processing_d_flags(va_list saved_variables,
- t_fnc_data *data, int negative)
+	t_fnc_data *data, int negative)
 {
 	if (data->width)
 		return (processing_d_width_flag(saved_variables, data, negative));
