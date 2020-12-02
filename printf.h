2d414044631f2136125a4fa72fed4461912c1098
Jean Vanden Bulck <jeanvandenbulck@MacBook-Pro-de-Jean.local>
2020-12-02 15:14:06 +0100

    Norme20

--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: jvanden- <jvanden-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 13:22:03 by jvanden-          #+#    #+#             */
-/*   Updated: 2020/12/02 15:12:41 by jvanden-         ###   ########.fr       */
+/*   Updated: 2020/12/02 15:14:01 by jvanden-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,35 +29,35 @@ typedef struct	s_fnc_data	{
 	char	conversion;
 }				t_fnc_data;
 
-int		ft_printf(const char *entry, ...);
-void	putstr(char *str);
-int		ft_strlen(char *str);
-char	*ft_itoa(int long long n, int base, char *basestr);
-char	*ft_strjoin_back(char *s1, char *s2);
-char	*ft_strjoin_front(char *s1, char *s2);
-char	*ft_strdup(char *s1);
-int		isnumber(char c);
-int		isinstr(char *str, char c);
-int		memory_allocation_error_free(void *to_free);
-int		processing_c(t_fnc_data *data);
-char	*create_filled_string(int length, char filler);
-int		processing_d(t_fnc_data *data);
-int		processing_d_flags(t_fnc_data *data, int negative);
-int		processing_s(t_fnc_data *data);
-char	*truncstr(char *oldstr, int range);
-int		processing_p(t_fnc_data *data);
-int		processing_u(t_fnc_data *data);
-int		processing_x(t_fnc_data *data);
-int		processing_cap_x(t_fnc_data *data);
-int		processing_percent(t_fnc_data *data);
-void	free_data(t_fnc_data *data);
-int		parsing(t_fnc_data *data, int start, int range, char *entry);
-char	*ft_itoa_pointer(unsigned long long int n, int base, char *basestr);
-int		processing_d_width_flag(t_fnc_data *data, int negative);
-int		processing_d_width_zero(t_fnc_data *data, int negative);
-int		processing_d_width_minus(t_fnc_data *data, int negative);
-int		processing_d_width_precision(t_fnc_data *data, int negative);
-int		processing_d_width_minus_precison(t_fnc_data *data, int negative);
-int		free_string_return(t_fnc_data *data, int i);
+int				ft_printf(const char *entry, ...);
+void			putstr(char *str);
+int				ft_strlen(char *str);
+char			*ft_itoa(int long long n, int base, char *basestr);
+char			*ft_strjoin_back(char *s1, char *s2);
+char			*ft_strjoin_front(char *s1, char *s2);
+char			*ft_strdup(char *s1);
+int				isnumber(char c);
+int				isinstr(char *str, char c);
+int				memory_allocation_error_free(void *to_free);
+int				processing_c(t_fnc_data *data);
+char			*create_filled_string(int length, char filler);
+int				processing_d(t_fnc_data *data);
+int				processing_d_flags(t_fnc_data *data, int negative);
+int				processing_s(t_fnc_data *data);
+char			*truncstr(char *oldstr, int range);
+int				processing_p(t_fnc_data *data);
+int				processing_u(t_fnc_data *data);
+int				processing_x(t_fnc_data *data);
+int				processing_cap_x(t_fnc_data *data);
+int				processing_percent(t_fnc_data *data);
+void			free_data(t_fnc_data *data);
+int				parsing(t_fnc_data *data, int start, int range, char *entry);
+char			*ft_itoa_pointer(unsigned long long int n, int base, char *basestr);
+int				processing_d_width_flag(t_fnc_data *data, int negative);
+int				processing_d_width_zero(t_fnc_data *data, int negative);
+int				processing_d_width_minus(t_fnc_data *data, int negative);
+int				processing_d_width_precision(t_fnc_data *data, int negative);
+int				processing_d_width_minus_precison(t_fnc_data *data, int negative);
+int				free_string_return(t_fnc_data *data, int i);
 
 #endif
